9fea944f75685e3effd9975af3fb87197a84de1b
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2026-04-11 20:41:25 +0100

    Added Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface BaseProps {
+		children: Snippet;
+		isSecondary?: boolean;
+		isDanger?: boolean;
+		isMenu?: boolean;
+	}
+
+	interface ButtonProps extends BaseProps {
+		onclick: (event: MouseEvent) => void;
+		href?: never;
+	}
+
+	interface LinkProps extends BaseProps {
+		href: string;
+		onclick?: never;
+	}
+
+	type ComponentProps = ButtonProps | LinkProps;
+
+	let { children, isDanger, isSecondary, isMenu, href, onclick, ...rest }: ComponentProps =
+		$props();
+</script>
+
+{#if href}
+	<a
+		{href}
+		{...rest}
+		class="btn"
+		class:btn-secondary={isSecondary}
+		class:btn-danger={isDanger}
+		class:btn-menu={isMenu}
+	>
+		{@render children()}
+	</a>
+{:else}
+	<button
+		{...rest}
+		{onclick}
+		class="btn"
+		class:btn-secondary={isSecondary}
+		class:btn-danger={isDanger}
+		class:btn-menu={isMenu}
+	>
+		{@render children()}
+	</button>
+{/if}
+
+<style>
+	a {
+		text-decoration: none;
+		display: block;
+	}
+
+	a:hover {
+		text-decoration: none;
+	}
+
+	.btn {
+		padding: 12px 24px;
+		min-width: 230px;
+		text-align: center;
+		background-color: black;
+		border-radius: 12px;
+		color: white;
+		border: 1px solid white;
+		font-weight: normal;
+		font-size: 22px;
+	}
+
+	.btn-secondary {
+		background-color: white;
+		color: black;
+		border: 1px solid black;
+	}
+
+	.btn-danger {
+		background-color: rgb(136, 4, 4);
+	}
+
+	.btn-menu {
+		min-width: 150px;
+		padding: 8px 20px;
+	}
+</style>
