584c918cd2b85bd07a8b8d575837fcb0100047fe
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2026-04-12 09:37:57 +0100

    Added Locals interface

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,17 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import type { Database } from '$lib/types/database.types';
+import { SupabaseClient, User } from '@supabase/supabase-js';
+
 declare global {
 	namespace App {
+		interface Locals {
+			supabase: SupabaseClient<Database>;
+			user: User | null;
+			safeGetSession: () => Promise<{ session: Session | null; user: User | null }>;
+			session: Session | null;
+		}
 		// interface Error {}
-		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
