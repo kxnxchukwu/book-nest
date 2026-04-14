fba233f76d174f7e52adf56a64d10d4440664add
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2026-04-14 12:34:26 +0100

    Added updateAccount and deleteAccount methods

--- src/lib/state/user-state.svelte.ts
+++ src/lib/state/user-state.svelte.ts
@@ -210,8 +210,57 @@ export class UserState {
 		}
 	}
 
+	async updateAccountData(email: string, userName: string) {
+		if (!this.session) {
+			return;
+		}
+
+		try {
+			const response = await fetch('/api/update-account', {
+				method: 'PATCH',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${this.session.access_token}`
+				},
+				body: JSON.stringify({
+					email,
+					userName
+				})
+			});
+
+			if (response.ok) {
+				this.userName = userName;
+			}
+		} catch (error) {
+			console.log(`Failed to delete account:`, error);
+		}
+	}
+
 	async logout() {
 		await this.supabase?.auth.signOut();
+	}
+
+	async deleteAccount() {
+		if (!this.session) {
+			return;
+		}
+
+		try {
+			const response = await fetch('/api/delete-account', {
+				method: 'DELETE',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${this.session.access_token}`
+				}
+			});
+
+			if (response.ok) {
+				await this.logout();
+				goto('/');
+			}
+		} catch (error) {
+			console.log('Failed to delete account:', error);
+		}
 	}
 }
 
