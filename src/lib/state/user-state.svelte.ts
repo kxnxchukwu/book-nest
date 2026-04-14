4d5b70f9ab9666577cc414df1aa7a8a4a7dba28d
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2026-04-14 11:28:24 +0100

    Added book related methods

--- src/lib/state/user-state.svelte.ts
+++ src/lib/state/user-state.svelte.ts
@@ -1,3 +1,4 @@
+import { goto } from '$app/navigation';
 import type { Database } from '$lib/types/database.types';
 import type { SupabaseClient, User, Session } from '@supabase/supabase-js';
 import { getContext, setContext } from 'svelte';
@@ -21,6 +22,12 @@ export interface Book {
 	title: string;
 	user_id: string;
 }
+
+export interface ScannedBook {
+	bookTitle: string;
+	author: string;
+}
+type UpdatableBookFields = Omit<Book, 'id' | 'user_id' | 'created_at'>;
 export class UserState {
 	session = $state<Session | null>(null);
 	supabase = $state<SupabaseClient<Database> | null>(null);
@@ -116,6 +123,91 @@ export class UserState {
 				const ratingZ = z.rating || 0;
 				return ratingZ - ratingA;
 			});
+	}
+
+	getBookById(bookId: number) {
+		return this.allBooks.find((book) => book.id === bookId);
+	}
+
+	async updateBook(bookId: number, updateObject: Partial<UpdatableBookFields>) {
+		if (!this.supabase) {
+			return;
+		}
+
+		const { status, error } = await this.supabase
+			.from('books')
+			.update(updateObject)
+			.eq('id', bookId);
+
+		if (status === 204 && !error) {
+			this.allBooks = this.allBooks.map((book) => {
+				if (book.id === bookId) {
+					return {
+						...book,
+						...updateObject
+					};
+				} else {
+					return book;
+				}
+			});
+		}
+	}
+
+	async uploadBookCover(file: File, bookId: number) {
+		if (!this.user || !this.supabase) {
+			return;
+		}
+
+		// Use Date.now() instead of new Date().getTime()
+		const filePath = `${this.user.id}/${Date.now()}_${file.name}`;
+
+		const { error: uploadError } = await this.supabase.storage
+			.from('book-covers')
+			.upload(filePath, file);
+
+		if (uploadError) {
+			return console.log(uploadError);
+		}
+
+		const {
+			data: { publicUrl }
+		} = this.supabase.storage.from('book-covers').getPublicUrl(filePath);
+
+		await this.updateBook(bookId, { cover_image: publicUrl });
+	}
+
+	async deleteBookFromLibrary(bookId: number) {
+		if (!this.supabase) {
+			return;
+		}
+
+		const { error, status } = await this.supabase.from('books').delete().eq('id', bookId);
+		if (!error && status === 204) {
+			this.allBooks = this.allBooks.filter((book) => book.id !== bookId);
+		}
+
+		goto('/private/dashboard');
+	}
+
+	async addBooksToLibrary(booksToAdd: ScannedBook[]) {
+		if (!this.supabase || !this.user) {
+			return;
+		}
+
+		const userId = this.user.id;
+
+		const processedBooks = booksToAdd.map((book) => ({
+			title: book.bookTitle,
+			author: book.author,
+			user_id: userId
+		}));
+
+		const { error } = await this.supabase.from('books').insert(processedBooks);
+		if (error) {
+			throw new Error(error.message);
+		} else {
+			await this.fetchUserData();
+		}
 	}
 
 	async logout() {
