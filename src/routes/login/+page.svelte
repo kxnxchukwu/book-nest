227b8635784f4f70b371ec194b22f3ed2e47e00b
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2026-04-14 17:39:16 +0100

    Added login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { AuthForm } from '$components';
+	import type { ActionData } from './$types';
+
+	let { form }: { form: ActionData } = $props();
+</script>
+
+<AuthForm isRegistration={false} {form} />
